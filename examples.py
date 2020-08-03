a7557cb39adc0e6ba9bcb7726d1d758285478950
octaviotastico <octaviopercivaldi2@gmail.com>
2020-08-03 18:29:00 -0300

    g7_e1 and g7_e2

--- examples.py
+++ examples.py
@@ -1,15 +1,43 @@
-import calculate_continue_distributions as dist
+import calculate_discrete_distributions as ddist
+import calculate_continue_distributions as cdist
+import simulate_discrete_variables as sdvar
+import simulate_continue_variables as scvar
 import arrays as arr
 import tests
 
+### Guia 7
+def g7_ex1():
+  # We already know the frequencys,
+  # we dont have to create an array
+  # s of length 564, so instead we
+  # create directly the Ni's (frequencys)
+
+  n = 564 # Total amount of samples
+  x = [ 'white', 'pink', 'red' ]
+  N = { 'white': 141, 'pink': 291, 'red': 132 }
+  p = [ 1/4, 2/4, 1/4 ]
+  stat, pval = tests.pearson_chi_squared_test(x, p, n, N=N)
+  print(f'Stat: {stat}, Pval: {pval}')
+
+def g7_ex2():
+  n = 1000
+  x = [ 1, 2, 3, 4, 5, 6 ]
+  N = { 1: 158, 2: 172, 3: 164, 4: 181, 5: 160, 6: 165 }
+  p = [ 1/6, 1/6, 1/6, 1/6, 1/6, 1/6 ]
+  stat, pval = tests.pearson_chi_squared_test(x, p, n, N=N)
+  print(f'Stat: {stat}, Pval: {pval}')
+
+g7_ex1()
+g7_ex2()
+
 def example_chi_1():
-  m = [1, 1, 1, 2, 3, 4, 5] # Muestra
+  s = [1, 1, 1, 2, 3, 4, 5] # Muestra
   x = [1, 2, 3, 4, 5] # Posibles X
   p = [0.3, 0.1, 0.2, 0.1, 0.3] # Probabilidades de x
-  print(tests.chi_squared_statistic(m, x, p))
+  print(tests.chi_squared_statistic(x, p, s))
 
 def example_chi_2():
-  m = [
+  s = [
     0, 0,
     1, 1, 1, 1, 1, 1, 1,
     2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
@@ -29,13 +57,13 @@ def example_chi_2():
     0.0546875, # Probabilidad de que salga un 6
     0.0078125  # Probabilidad de que salga un 7
   ]
-  print(tests.pearson_chi_squared_test(m, x, p, 0.05))
+  print(tests.pearson_chi_squared_test(x, p, len(s), s))
 
 # We don't know the mean
 # Example 8.2 page 138
 def example_chi_3():
   x = [0, 1, 2, 3, 4, 5] # Where 5 actually a group with all values greater than 5.
-  m = [
+  s = [
     0, 0, 0, 0, 0, 0,
     1, 1,
     2,
@@ -43,25 +71,25 @@ def example_chi_3():
     4, 4, 4, 4, 4, 4, 4,
     5, 5, 5, 5, 8
   ] # Experimental data.
-  tetha = arr.mu(m) # Estimates Lambda, calculated by the experimental data.
+  tetha = arr.mu(s) # Estimates Lambda, calculated by the experimental data.
   p = [
-    dist.poisson_PDF(tetha, 0), # P(X = 0) ningun accidente
-    dist.poisson_PDF(tetha, 1), # P(X = 1) 1 accidente
-    dist.poisson_PDF(tetha, 2), # P(X = 2) 2 accidentes
-    dist.poisson_PDF(tetha, 3), # P(X = 3) 3 accidentes
-    dist.poisson_PDF(tetha, 4), # P(X = 4) 4 accidentes
-    1 - dist.poisson_CDF(tetha, 5)
+    cdist.poisson_PDF(tetha, 0), # P(X = 0) ningun accidente
+    cdist.poisson_PDF(tetha, 1), # P(X = 1) 1 accidente
+    cdist.poisson_PDF(tetha, 2), # P(X = 2) 2 accidentes
+    cdist.poisson_PDF(tetha, 3), # P(X = 3) 3 accidentes
+    cdist.poisson_PDF(tetha, 4), # P(X = 4) 4 accidentes
+    1 - cdist.poisson_CDF(tetha, 5)
   ]
   N = { 0: 6, 1: 2, 2: 1, 3: 9, 4: 7, 5: 5 }
   # H0 is: Data is from Poisson distribution.
-  p = tests.pearson_chi_squared_test_unknown_params(m, x, p, 0.05, 1, N, 30, True)
+  p = tests.pearson_chi_squared_test_unknown_params(s, x, p, 0.05, 1, N, 30, True)
   print(p)
 
 def example_kolsmir_1():
   kolmogorov_smirnov(
     [3, 1, 2, 5],
     1000,
-    dist.exponential_CDF,
+    cdist.exponential_CDF,
     1/3
   )
 
@@ -69,6 +97,6 @@ def example_kolsmir_2():
   kolmogorov_smirnov(
     [55, 72, 81, 94, 112, 116, 124, 140, 145, 155],
     10000,
-    dist.exponential_CDF,
+    cdist.exponential_CDF,
     1/100
   )
--- tests.py
+++ tests.py
@@ -5,56 +5,72 @@ import numpy as np
 ########## Chi-squared Pearson's test ##########
 
 # Returns chi squared T statistic
-def chi_squared_statistic(m, x, p):
-  N, T, n = frequency(m), 0, len(m)
+def chi_squared_statistic(x, p, s=None, N=None):
+  N, T, n = frequency(s), 0, len(s)
   for i in range(len(x)):
     T += (((N[x[i]] if x[i] in N.keys() else 0) - n*p[i])**2) / (n*p[i])
   return T
 
 # Returns chi squared T statistic
-def chi_squared_statistic_with_agrupation(N, n, x, p):
+def chi_squared_statistic_agrupation(N, n, x, p):
   T = 0
   for i in range(len(x)):
     T += (((N[x[i]] if x[i] in N.keys() else 0) - n*p[i])**2) / (n*p[i])
   return T
 
-# Simulates p-value for a Pearson chi square
-# test for a discrete random variable.
-# If p-value is very similar to alpha, CREATE
-# new m's (samples), and call this function
-# multiple times. Count the proportion (how
-# many) of the p-values are lower than alpha,
-# and take a new desition (for example, if 60%
-# of pvals are lower, then reject it)
-def pearson_chi_squared_test(m, x, p, alpha):
-  T = chi_squared_statistic(m, x, p)
-  p_value = dist.chi_square_CDF(len(x) - 1, T)
-  return p_value, 'Reject H0, pval < alfa' if (p_value <= alpha) else 'Dont reject H0, pval > alfa'
+# Simulates p-value using Pearson's chi square
+# test for a discrete random variable, using
+# the T statistic created by chi_squared_statistics
+# functions above. Should be equal to scipy.stats.chisquare
+def pearson_chi_squared_test(x, p, n, s=None, N=None):
+  # If p-value is very similar to alpha, CREATE new s's (samples), and call this function
+  # multiple times. Count the proportion (how many) of the p-values are lower than alpha,
+  # and take a new desition (for example, if 60% of pvals are lower, then reject it)
+  if s:
+    T = chi_squared_statistic(x, p, s)
+  else:
+    T = chi_squared_statistic_agrupation(N, n, x, p)
+  return T, dist.chi_square_CDF(len(x) - 1, T)
 
 # If we have any unknown parameter in our distribution,
 # we have to substract the amount of them in the
 # degrees of freedom on the chi squared cdf.
-def pearson_chi_squared_test_unknown_params(m, x, p, alpha, unknown_params, N=None, n=0, agrupation=False):
-  T = chi_squared_statistic_with_agrupation(N, n, x, p) if agrupation else chi_squared_statistic(m, x, p)
+def pearson_chi_squared_test_unknown_params(s, x, p, alpha, unknown_params, N=None, n=0, agrupation=False):
+  T = chi_squared_statistic_agrupation(N, n, x, p) if agrupation else chi_squared_statistic(x, p, s)
   p_value = dist.chi_square_CDF(len(x) - 1 - unknown_params, T)
   return p_value, p_value <= alpha
 
 ########## Kolmogorov-Smirnov Test ##########
 
+# Calculates the empirical distribution for some
+# array x of data taken from an experiment
 def empirical_dist(x):
   Fe = frequency(x)
   for elem in Fe:
     Fe[elem] /= len(x)
   return np.insert(np.cumsum(list(Fe.values())), 0, 0, axis=0)
 
-def kolmogorov_smirnov(m, sims, dist, *params):
-  Fe = empirical_dist(m)
+def get_dks(s, Fe, dist, *params):
+  s.sort()
+  for i in range(len(s)):
+    F = dist(*params, s[i])
+    d_original = max(Fe[i+1] - F, F - Fe[i], d_original)
+
+# This function calculates the p-value of a sample s
+# in which we already KNOW all its parameters.
+# The Kolmogorov Smirnov test creates a 'd' statistic
+# comparing the H0 distribution values with the empiric
+# distribution ones, then it gets the proportion of how many
+# of some {sims} new d_i statistics created with a uniform
+# distribution are greather than the original d statistic
+def kolmogorov_smirnov(s, sims, dist, *params):
+  Fe = empirical_dist(s)
   d_original, p_value = 0, 0
-  size = len(m)
-  m = list(set(m))
-  m.sort()
-  for i in range(len(m)):
-    F = dist(*params, m[i])
+  size = len(s)
+  s = list(set(s))
+  s.sort()
+  for i in range(len(s)):
+    F = dist(*params, s[i])
     d_original = max(Fe[i+1] - F, F - Fe[i], d_original)
   print('K statistic', d_original)
   for i in range(sims):
@@ -67,3 +83,21 @@ def kolmogorov_smirnov(m, sims, dist, *params):
       p_value += 1
   print('p-value', p_value/sims)
   return p_value/sims
+
+# This function calculates the p-value of a sample s
+# in which we DONT KNOW some of its parameters.
+
+# Mucho viaje, probably wont do it, porque hacerlo para
+# un caso en especifico es re facil, y no se si vale la pena :/
+
+# def kolmogorov_smirnov_unknown_params(s, sims, alpha, dist, *params):
+#   size = len(s)
+#   p_val = kolmogorov_smirnov(s, sims, dist, *params)
+#   if p_val <= alpha:
+
+#     for i in range(sims):
+#       sample = []
+#       for i in range(size):
+#         sample.append(dist(params))
+#       unknown_params = get_unknown_params(dist, *params)
+
